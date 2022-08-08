9258535be01e49b43e1287b037266babdd81bbd0
pythonmeraklisi10 <104316540+pythonmeraklisi10@users.noreply.github.com>
2022-08-08 19:58:32 +0300

    Create facedetecting.py

---
+++ facedetecting.py
@@ -0,0 +1,42 @@
+import cv2,pyttsx3,threading,os
+def mouse(event,x,y,flags,params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print((x,y))
+
+def say(word):
+    try:
+        engine.say(word)
+        engine.runAndWait()
+    except:
+        pass
+        
+engine = pyttsx3.init()
+cv2.namedWindow("Kamera")
+cv2.setMouseCallback("Kamera",mouse)
+cas = cv2.CascadeClassifier("./yapay zeka/face.xml")
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret,img = cap.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    faces = cas.detectMultiScale(gray,1.1,4)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+        cv2.line(img,(x+w//2,y),(x+w//2,y-50),(0,0,255),3)
+        cv2.line(img,(x,y+h//2),(x-50,y+h//2),(0,0,255),3)
+        cv2.line(img,(x+w//2,y+h),(x+w//2,y+h+50),(0,0,255),3)
+        cv2.line(img,(x+w,y+h//2),(x+w+50,y+h//2),(0,0,255),3)
+        t = threading.Thread(target = say,args = ("Human face detected",))
+        t.start()
+        os.system("cls")
+        print(f"""
+{(x,y)}-----{(x+w,y)}
+|                    |
+|     Human Face     |
+|                    |
+{(x,y+h)}-----{(x+w,y+h)}""")
+    cv2.imshow("Kamera",img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
